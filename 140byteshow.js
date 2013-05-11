84ee011210f81c2eea4fb7e2084fe6b60aca26e1
Ben Smithett <bsmithett@gmail.com>
2013-05-12 09:26:19 +1000

    Shift some duplication

--- 140byteshow.js
+++ 140byteshow.js
@@ -1,1 +1,1 @@
-s=0;d=document.body;l=d.children.length-1;onkeyup=function(e){d.id="s"+(e.keyCode>38?s<l-1?++s:l-1:s>0?--s:0)}
+s=0;d=document.body;l=d.children.length-2;onkeyup=function(e){d.id="s"+(e.keyCode>38?s<l?++s:l:s>0?--s:0)}
