55932f8c429eb3b466005ab8754a5464613216b0
Christopher Giffard <christopher.giffard@cgiffard.com>
2013-08-15 12:19:06 +1000

    Moved back to a sensible key which works in most browsers

--- 140byteshow.js
+++ 140byteshow.js
@@ -1,1 +1,1 @@
-s=0;d=document.body;l=d.children.length-2;k=onkeyup=function(e){d.id="s"+(e.keyCode<9?s<l?++s:l:s>0?--s:0)};k({})
+s=0;d=document.body;l=d.children.length-2;(onkeyup=function(e){d.id="s"+(e.keyCode>38?s<l?++s:l:s>0?--s:0)})({})
